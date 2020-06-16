849e74cd56700fdf76390eef10df51a0b772320e
Mukesh Yadav <mukeshyadav@gmail.com>
2020-06-16 21:28:58 +0530

    adding websocket, form and list page

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,30 +1,44 @@
 <script>
-	export let name;
+  import { onMount } from "svelte";
+  import MsgForm from "./MsgForm.svelte";
+  import MsgList from "./MsgList.svelte";
+
+  import { testWebSocket } from "./ws";
+  let websocket;
+  const sendMessage = ({ detail }) => {
+    websocket.send(detail.text);
+  };
+  const messages = [];
+
+  onMount(() => {
+    testWebSocket();
+  });
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
+
+<main>
+  <h1>Svelte Chat!</h1>
+  <MsgList {messages} />
+  <MsgForm on:send={sendMessage} />
+</main>
---
+++ svelte-app/src/MsgForm.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
+  let text = "";
+  let disableFormSending = true;
+  // reactive statement
+  $: disableFormSending = text.length > 5;
+
+  const sendMessage = () => {
+    dispatch("send", {
+      text
+    });
+  };
+</script>
+
+<input bind:value={text} />
+<button on:click={sendMessage} disabled={!disableFormSending}>Send</button>
